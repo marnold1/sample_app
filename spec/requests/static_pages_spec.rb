b0e520660ba4cce81135ab85c27099f1603ba5a0
marnold1 <mr.arnold@gmail.com>
2012-06-07 22:44:49 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  it "should have the right title" do
+	  visit '/static_pages/home'
+	  page.should have_selector('title',
+	                    :text => "Ruby on Rails Tutorial Sample App | Home")
+	end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  it "should have the right title" do
+	  visit '/static_pages/help'
+	  page.should have_selector('title',
+	                    :text => "Ruby on Rails Tutorial Sample App | Help")
+	end
+
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
+    it "should have the right title" do
+	  visit '/static_pages/about'
+	  page.should have_selector('title',
+	                    :text => "Ruby on Rails Tutorial Sample App | About")
+	end
+end
